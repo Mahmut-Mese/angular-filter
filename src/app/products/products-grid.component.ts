16f292e2e411d24a2241c14936e07c02357e6192
IR <na@intelligentreach.com>
2021-05-03 13:53:35 +0100

    products

--- src/app/products/products-grid.component.ts
+++ src/app/products/products-grid.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "products-table",
   templateUrl: "products-grid.component.html",
   styleUrls: [],
-  providers: []
+  providers: [],
 })
 export class ProductsGridComponent {
-
+  @Input() products: [];
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core";
 
 import { ProductsGridComponent } from "./products-grid.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-  imports: [
-  ],
-  declarations: [
-    ProductsGridComponent
-  ],
-  exports: [
-    ProductsGridComponent
-  ],
-  providers: [
-  ]
+  imports: [BrowserModule],
+  declarations: [ProductsGridComponent],
+  exports: [ProductsGridComponent],
+  providers: [],
 })
-
-export class ProductsModule { }
+export class ProductsModule {}
